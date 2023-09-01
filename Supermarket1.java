b46d6e85e8296dd4e4366327907768f77bf4b267
guganbharathi <guganbharathi11@gmail.com>
2023-09-01 11:11:58 +0530

    void

---
+++ Supermarket1.java
@@ -0,0 +1,20 @@
+public class Supermarket1
+{
+
+public static void main(String[] args)
+{
+ Supermarket1 sm= new Supermarket1();
+              int re =sm.add();
+	          System.out.println(re);
+	
+}
+ 
+int add()
+{
+		int no1=10;
+		int no2=20;
+		int result =no1+no2;	
+	System.out.println(result);
+		return result;
+ }
+}
