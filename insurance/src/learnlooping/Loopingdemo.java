ce1e0207981dad99c03b71e35a81064b7ca62543
guganbharathi <guganbharathi11@gmail.com>
2023-09-28 12:15:09 +0530

    looping

---
+++ insurance/src/learnlooping/Loopingdemo.java
@@ -0,0 +1,13 @@
+package learnlooping;
+
+public class Loopingdemo {
+ 	public static void main(String[] args) {
+		int count=1;
+          while(count<=5) {
+			System.out.println(1+" ");
+			count =count+1;
+			
+			
+	}
+ 	}
+}
